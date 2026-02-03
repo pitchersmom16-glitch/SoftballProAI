f51cc06b1487636ba993afb00aec9bc7dd4473ac
pitchersmom16-glitch <pitchersmom16@gmail.com>
2026-02-02 22:18:51 -0500

    wip: backup before remote-restore

---
+++ types/shims_shared_routes.d.ts
@@ -0,0 +1,11 @@
+﻿declare module '@shared/routes' {
+  export const api: any;
+  export const buildUrl: any;
+  export type Assessment = any;
+  export type CreateAssessmentRequest = any;
+  export type Coach = any;
+  export type CreateCoachRequest = any;
+  export type Team = any;
+  export type CreateTeamRequest = any;
+  export default any;
+}
